e41bb4bd0eec6522c7b1ae4fac6dde5a5b3ef394
devsamuelsilva <devsamuelsilva@gmail.com>
2023-10-09 09:20:02 -0300

    Finalizando o exercicio de esplicar sobre 3 Methods do JS.

--- javascript/atividade/index.js
+++ javascript/atividade/index.js
@@ -9,7 +9,7 @@
 // Primeiro comecamos com a declarar um Array de Objetos para usarmos os Methods.
 
 const dados = [
-    {nome: "Samuel", idade: 33, salario: 7800, area: "Centro de Controle"},
+    {nome: "Samuel", idade: 33, salario: 7800, area: "Centro de Controle Pleno"},
     {nome: "Alexandre", idade: 37, salario: 10800, area: "Centro de Controle Senior"},
     {nome: "Dayane", idade: 31, salario: 13500, area: "Coordenadora de O&M"},
     {nome: "Lucas", idade: 32, salario: 9000, area: "Analista de Pré e Pós Operação"},
@@ -21,11 +21,11 @@ const dados = [
  */
 
 // EX:
-const exFind = dados.find((salary) => {
-    return salary.salario >= 8000;
-})
+// const exFind = dados.find((salary) => {
+//     return salary.salario >= 8000;
+// })
 // Imprmindo o resultado. No caso ele vai percorrer o ARRAY e Trazer APENAS o PIRMEIRO iten que está dentro da Condicao >=8000
-console.log(exFind); // Neste caso ele me retorna o Alexandre pois ele e o primeiro que atende a condicao >= 8000.
+// console.log(exFind); // Neste caso ele me retorna o Alexandre pois ele e o primeiro que atende a condicao >= 8000.
 
 /** 2 Methods e o FILTER.
  *  O Method do Filter ele vai percorer o array de objetos e retornar um array com todos os objetos que atedem a condicao especificada
@@ -33,9 +33,26 @@ console.log(exFind); // Neste caso ele me retorna o Alexandre pois ele e o prime
  */
 
 //EX:
-const exFilter = dados.filter((age) => {
-    return age.idade <= 33;
-})
+// const exFilter = dados.filter((age) => {
+//     return age.idade <= 33 //&& age.salario >=10000;
+// })
 // Neste caso com foi usar o Filter ele vai olhar todos os itens do Array e traxxer em outro array tudo que atende a condicao.
-console.log(exFilter); // vai traze o samuel, dayane e lucas.
+// Neste caso ele pode atender a mais de uma condicao como no exemplo
+// console.log(exFilter); // vai traze o samuel, dayane e lucas. sem o "&& age.salario >=10000"
 
+/** 3 Methods e o FINDINDEX.
+ *  Encontra a posicao de um iten dentro do array
+ */
+
+const exFinder = dados.findIndex((area) => {
+    return area.area === "Centro de Controle Pleno"
+})
+
+// apos achar a pocisao do dado no array conseguimos manipular ele
+console.log(exFinder);
+// por exemplo peguei a possicao com o findIndex e alterei a AREA
+console.log(dados[exFinder].area = "Centro de Controle Senior");
+//aqui quando exibo novamente ele me retorna o array agora com o samuel como Operador de centro de controle Senior
+console.log(dados);
+
+//FIM.
